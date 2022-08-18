57b4a7a76fdb78473fa41818075a25354e1817aa
Asankilp <3384816498@qq.com>
2022-08-18 14:37:36 +0800

    Initial commit

---
+++ src/main/java/io/github/asankilp/dys/Dys.java
@@ -0,0 +1,26 @@
+package io.github.asankilp.dys;
+import io.github.asankilp.dys.item.ItemReg;
+import io.github.asankilp.dys.sound.SoundReg;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Dys.MODID)
+public class Dys {
+    public static final String MODID = "dys";
+    public Dys() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+        ItemReg.BLOCKS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ItemReg.ITEMS.register(modEventBus);
+        SoundReg.SOUNDS.register(modEventBus);
+    }
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+
+    }
+}
